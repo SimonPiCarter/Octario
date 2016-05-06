e54370518f9f46b39d31dbe9e3bb1e8041b25775
simon.pierre <sh.pierre@laposte.net>
2016-05-06 12:24:56 +0200

    Added : Singleton

---
+++ core/Tools/Singleton.h
@@ -0,0 +1,42 @@
+#pragma once
+
+template <class T>
+class Singleton
+{
+    public:
+        static T& Get();
+    protected:
+
+		static T _Data;
+};
+
+/*** EXAMPLE
+>>HPP
+	class Window : public Singleton<Window>
+	{
+		friend class Singleton<Window>;
+
+		public:
+
+		protected:
+			Window();
+			~Window();
+	}
+
+>>CPP
+	template <class T> T Singleton<T>::_Data = T();
+
+	template <class T> T& Singleton<T>::Get()
+	{
+		return _Data;
+	}
+***/
+
+template <class T> T Singleton<T>::_Data = T();
+
+template <class T> T& Singleton<T>::Get()
+{
+    return _Data;
+}
+
+
